2fd49031d82f4f776efad4ee44747f3a6b79b331
qb4745 <jai.vicencio@duocuc.cl>
2025-09-06 22:07:57 -0400

    spring scaffold

---
+++ src/main/java/com/cambiaso/iocbackend/IocbackendApplication.java
@@ -0,0 +1,13 @@
+package com.cambiaso.iocbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IocbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IocbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cambiaso/iocbackend/IocbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cambiaso.iocbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IocbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
