329fcfc5e6015b17fbd7d361e8d3e84cfa7b5360
qb4745 <jai.vicencio@duocuc.cl>
2025-09-08 23:21:42 -0300

    supabase configurado

--- src/main/java/com/cambiaso/ioc/IocbackendApplication.java
+++ src/main/java/com/cambiaso/ioc/IocbackendApplication.java
@@ -1,4 +1,4 @@
-package com.cambiaso.iocbackend;
+package com.cambiaso.ioc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/cambiaso/ioc/IocbackendApplicationTests.java
+++ src/test/java/com/cambiaso/ioc/IocbackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.cambiaso.iocbackend;
+package com.cambiaso.ioc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
